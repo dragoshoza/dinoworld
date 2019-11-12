7ebcda3a1885b2ba35e61fafcbd5f3e1872c02de
Dragos Hoza <you@example.com>
2019-11-12 08:56:44 +0200

    First commit

---
+++ DinoWorld-Proto/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+    public Camera camera;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        Instantiate(myPrefab,camera.transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        Instantiate(myPrefab, camera.transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
